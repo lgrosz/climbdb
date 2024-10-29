813321088c0985c02398c22df14c17b66c7b3cd2
Logan Grosz <logan.grosz@gmail.com>
2024-10-28 20:23:24 -0600

    test: verify function and trigger deployments
    This uses the built-in function
    [`pg_get_functiondef`](https://www.postgresql.org/docs/current/functions-info.html)
    to verify function existence. It throws if the function doesn't exist.
    Triggers are a bit tricker since they are made up a many things
    including a particular table and function. For this a PL/pgSQL function
    is used where a query is made against the [`pg_trigger`
    table](https://www.postgresql.org/docs/current/catalog-pg-trigger.html)
    and asserted for existence.

--- verify/climb-font-grades.sql
+++ verify/climb-font-grades.sql
@@ -6,4 +6,20 @@ SELECT id, climb_id, value, plus, letter
     FROM climb_font_grades
     WHERE FALSE;
 
+SELECT pg_get_functiondef('check_font_letter()'::regprocedure);
+
+DO $$
+BEGIN
+    ASSERT (
+        SELECT EXISTS (
+            SELECT 1
+            FROM pg_trigger
+            WHERE
+                tgname = 'enforce_letter_constraint' AND
+                tgrelid = 'climb_font_grades'::regclass AND
+                tgfoid = 'check_font_letter'::regproc
+        )
+    );
+END $$;
+
 ROLLBACK;
--- verify/formation-closures.sql
+++ verify/formation-closures.sql
@@ -6,8 +6,20 @@ SELECT formation_id, super_formation_id, super_area_id
     FROM formation_closures
     WHERE FALSE;
 
--- TODO Verify function existence
+select pg_get_functiondef('check_formation_closures_cycle()'::regprocedure);
 
--- TODO Verify trigger existence
+DO $$
+BEGIN
+    ASSERT (
+        SELECT EXISTS (
+            SELECT 1
+            FROM pg_trigger
+            WHERE
+                tgname = 'prevent_formation_closures_cycle' AND
+                tgrelid = 'formation_closures'::regclass AND
+                tgfoid = 'check_formation_closures_cycle'::regproc
+        )
+    );
+END $$;
 
 ROLLBACK;
