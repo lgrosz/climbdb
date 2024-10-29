a1ea3a811e57d0b32fb08eef8b66f1b4cd426489
Logan Grosz <logan.grosz@gmail.com>
2024-10-28 20:23:24 -0600

    test: verify function and trigger deployments
    This uses the built-in function
    [`pg_get_functiondef`](https://www.postgresql.org/docs/current/functions-info.html)
    to verify function existence. It throws if the function doesn't exist.
    Triggers are a bit tricker since they are made up a many things
    including a particular table and function. For this a PL/pgSQL function
    is used where a query is made against the [`pg_trigger`
    table](https://www.postgresql.org/docs/current/catalog-pg-trigger.html)
    and asserted for existence.

--- verify/area-closures.sql
+++ verify/area-closures.sql
@@ -6,8 +6,20 @@ SELECT area_id, super_area_id
     FROM area_closures
     WHERE FALSE;
 
--- TODO Verify function existence
+SELECT pg_get_functiondef('check_area_closures_cycle()'::regprocedure);
 
--- TODO Verify trigger existence
+DO $$
+BEGIN
+    ASSERT (
+        SELECT EXISTS (
+            SELECT 1
+            FROM pg_trigger
+            WHERE
+                tgname = 'prevent_area_closures_cycle' AND
+                tgrelid = 'area_closures'::regclass AND
+                tgfoid = 'check_area_closures_cycle'::regproc
+        )
+    );
+END $$;
 
 ROLLBACK;
--- verify/climb-yds-grades.sql
+++ verify/climb-yds-grades.sql
@@ -6,4 +6,20 @@ SELECT id, climb_id, value, letter
     FROM climb_yds_grades
     WHERE FALSE;
 
+SELECT pg_get_functiondef('check_yds_letter()'::regprocedure);
+
+DO $$
+BEGIN
+    ASSERT (
+        SELECT EXISTS (
+            SELECT 1
+            FROM pg_trigger
+            WHERE
+                tgname = 'enforce_letter_constraint' AND
+                tgrelid = 'climb_yds_grades'::regclass AND
+                tgfoid = 'check_yds_letter'::regproc
+        )
+    );
+END $$;
+
 ROLLBACK;
