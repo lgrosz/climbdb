3d76fc2b9caddc53beb11c0c59984006bfcf0949
Logan Grosz <logan.grosz@gmail.com>
2024-12-31 12:24:10 -0700

    chore: remove climb-description-type-defaults migration
    This was mistakenly committed from a working branch before the
    description column was added to each entity.

--- deploy/climb-description-type-defaults.sql
+++
@@ -1,9 +0,0 @@
--- Deploy climb-pg:climb-description-types-defaults to pg
--- requires: climb-description-types
-
-BEGIN;
-
-INSERT INTO climb_description_types(name)
-    VALUES ('description');
-
-COMMIT;
