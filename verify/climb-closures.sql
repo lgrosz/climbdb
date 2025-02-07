2fa465c5b248efd75a5914238749131b7065def6
Logan Grosz <logan.grosz@gmail.com>
2025-02-06 21:19:41 -0700

    refactor!: relax mutual exclusivity checks
    When a closure from a to b exists, one may want to attempt to insert
    another closure from a to b with the same primary key and perform an
    update when the primary-key conflict occurs. This currently isn't
    possible because the trigger function checks all possible parents
    instead so the trigger function will raise before the primary-key
    constraint will.
    
    This patch splits the responsibility of the trigger function into
    multiple trigger functions that only raise if one table contains a
    matching row, effectively relaxing the exclusivity pre-check condition.
    
    This does change expected, likely edge-case, behavior slightly.

--- verify/climb-closures.sql
+++ verify/climb-closures.sql
@@ -10,7 +10,8 @@ SELECT climb_id, super_formation_id
     FROM climb_super_formation_closures
     WHERE FALSE;
 
-SELECT pg_get_functiondef('check_climb_closure_mutual_exclusivity()'::regprocedure);
+SELECT pg_get_functiondef('check_no_climb_super_area_closure()'::regprocedure);
+SELECT pg_get_functiondef('check_no_climb_super_formation_closure()'::regprocedure);
 
 DO $$
 BEGIN
@@ -21,7 +22,7 @@ BEGIN
             WHERE
                 tgname = 'enforce_climb_super_area_closure_mutual_exclusivity' AND
                 tgrelid = 'climb_super_area_closures'::regclass AND
-                tgfoid = 'check_climb_closure_mutual_exclusivity'::regproc
+                tgfoid = 'check_no_climb_super_formation_closure'::regproc
         )
     );
 
@@ -32,7 +33,7 @@ BEGIN
             WHERE
                 tgname = 'enforce_climb_super_formation_closure_mutual_exclusivity' AND
                 tgrelid = 'climb_super_formation_closures'::regclass AND
-                tgfoid = 'check_climb_closure_mutual_exclusivity'::regproc
+                tgfoid = 'check_no_climb_super_area_closure'::regproc
         )
     );
 END $$;
