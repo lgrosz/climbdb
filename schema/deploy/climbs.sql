4261f54ea1d567a6387041d17c1828853ab6f405
Logan Grosz <logan.grosz@gmail.com>
2025-08-17 14:32:28 -0600

    feat(climbs): add "parent" foreign keys
    Similar to the formations columns, but has an additional formation_id
    column. Moves the climb change to _after_ the formation change.

--- schema/deploy/climbs.sql
+++ schema/deploy/climbs.sql
@@ -1,12 +1,25 @@
 -- Deploy climbdb:climbs to pg
 -- requires: appschema
+-- requires: regions
+-- requires: crags
+-- requires: sectors
+-- requires: formations
 
 BEGIN;
 
 CREATE TABLE climb.climbs (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT,
-    description TEXT
+    description TEXT,
+
+    -- possible "parents"
+    region_id INTEGER REFERENCES climb.regions(id),
+    crag_id INTEGER REFERENCES climb.crags(id),
+    sector_id INTEGER REFERENCES climb.sectors(id),
+    formation_id INTEGER REFERENCES climb.formations(id),
+
+    -- exclusive-or parent enforcement
+    CONSTRAINT at_most_one_parent CHECK ( num_nonnulls(region_id, crag_id, sector_id, formation_id) < 2 )
 );
 
 COMMIT;
--- schema/verify/climbs.sql
+++ schema/verify/climbs.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-SELECT id, name, description
+SELECT id, name, description, region_id, crag_id, sector_id, formation_id
     FROM climb.climbs
     WHERE FALSE;
 
