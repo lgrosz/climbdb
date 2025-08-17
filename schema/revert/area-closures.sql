9e3df9d92972f3424e46cb3b88039909c4fc0d93
Logan Grosz <logan.grosz@gmail.com>
2025-08-17 11:36:22 -0600

    refactor!: remove closure tables
    I am going to do this differently.

--- schema/revert/area-closures.sql
+++
@@ -1,9 +0,0 @@
--- Revert climbdb:area-closures from pg
-
-BEGIN;
-
-DROP TRIGGER prevent_area_closures_cycle ON area_closures;
-DROP FUNCTION check_area_closures_cycle();
-DROP TABLE area_closures;
-
-COMMIT;
