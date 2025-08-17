136386d9b8714371e8c4e379458c41ee57bac7c2
Logan Grosz <logan.grosz@gmail.com>
2025-08-17 11:36:54 -0600

    refactor!: remove areas
    Use regions, crags, and sectors instead.

--- schema/revert/areas.sql
+++
@@ -1,7 +0,0 @@
--- Revert climbdb:areas from pg
-
-BEGIN;
-
-DROP TABLE areas;
-
-COMMIT;
--- schema/verify/areas.sql
+++
@@ -1,9 +0,0 @@
--- Verify climbdb:areas on pg
-
-BEGIN;
-
-SELECT id, name, description
-    FROM areas
-    WHERE FALSE;
-
-ROLLBACK;
