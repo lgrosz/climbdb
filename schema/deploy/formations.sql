880a6d5bda8beeaefee1e3741aee18daa58c6eec
Logan Grosz <logan.grosz@gmail.com>
2025-08-17 13:42:22 -0600

    feat(formations): add "parent" foreign keys
    A formation can be long to a region, a crag, a sector, or none,
    exclusively. This patch adds a column for each of the entities and a
    named constraint check to enforce this "at most one parent" idea. This
    check is necessary as it prevents nonsensical data from entering the
    table.
    
    Note that the test does not check for a named constraint as that is
    (currently) a limitation of pgTAP.

--- schema/deploy/formations.sql
+++ schema/deploy/formations.sql
@@ -1,6 +1,9 @@
 -- Deploy climbdb:formations to pg
 -- requires: appschema
 -- requires: postgis
+-- requires: regions
+-- requires: crags
+-- requires: sectors
 
 BEGIN;
 
@@ -8,7 +11,15 @@ CREATE TABLE climb.formations (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT,
     description TEXT,
-    location geometry(POINT, 4326)
+    location geometry(POINT, 4326),
+
+    -- possible "parents"
+    region_id INTEGER REFERENCES climb.regions(id),
+    crag_id INTEGER REFERENCES climb.crags(id),
+    sector_id INTEGER REFERENCES climb.sectors(id),
+
+    -- exclusive-or parent enforcement
+    CONSTRAINT at_most_one_parent CHECK ( num_nonnulls(region_id, crag_id, sector_id) < 2 )
 );
 
 COMMIT;
