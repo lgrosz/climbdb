0129d18e64d655a671e55eac6ca656ca688b9d33
Logan Grosz <logan.grosz@gmail.com>
2025-10-08 16:30:02 -0600

    feat(seeds): add ascent members seed data

---
+++ seeds/deploy/ascent-members.sql
@@ -0,0 +1,35 @@
+-- Deploy climbdb-seeds:ascent-members to pg
+-- requires: climbers
+-- requires: ascents
+-- requires: climbdb:climb/ascent-members
+
+BEGIN;
+
+CREATE TABLE ascent_member_seed_refs (
+    ref TEXT PRIMARY KEY,
+    ascent_id INTEGER NOT NULL,
+    climber_id INTEGER NOT NULL,
+    FOREIGN KEY (ascent_id, climber_id)
+        REFERENCES climb.ascent_members(ascent_id, climber_id)
+        ON DELETE CASCADE
+);
+
+CREATE FUNCTION ascent_member_seed_ref(p_ref TEXT)
+RETURNS TABLE(ref TEXT, ascent_id INT, climber_id INT) LANGUAGE sql STABLE AS $$
+    SELECT ref, ascent_id, climber_id
+    FROM ascent_member_seed_refs
+    WHERE ref = p_ref;
+$$;
+
+WITH last_ascent_member AS (
+    INSERT INTO climb.ascent_members (
+        ascent_id,
+        climber_id
+    ) VALUES (
+        (SELECT ascent_id FROM ascent_seed_ref('atomic-decay-fa')),
+        (SELECT climber_id FROM climber_seed_ref('josh-dreher'))
+    ) RETURNING ascent_id, climber_id
+) INSERT INTO ascent_member_seed_refs (ref, ascent_id, climber_id)
+    SELECT 'atomic-decay-fa-josh-dreher', ascent_id, climber_id FROM last_ascent_member;
+
+COMMIT;
