915c5114c45fe9c40018d2282aafbdbc7a33d208
Logan Grosz <logan.grosz@gmail.com>
2026-03-29 11:16:46 -0600

    feat!: remove all media-related schema
    Out of scope for this database.

--- schema/revert/climb/formations-in-image.sql
+++
@@ -1,7 +0,0 @@
--- Revert climbdb:climb/formations-in-image from pg
-
-BEGIN;
-
-DROP TABLE climb.formations_in_image;
-
-COMMIT;
--- schema/verify/media/images.sql
+++
@@ -1,9 +0,0 @@
--- Verify climbdb:media/images on pg
-
-BEGIN;
-
-SELECT id, alt, object_key
-    FROM media.images
-    WHERE FALSE;
-
-ROLLBACK;
