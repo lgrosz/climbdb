366ea8e2088a673c41c3c1ff7bc33844d3ebd554
Logan Grosz <logan.grosz@gmail.com>
2026-03-29 21:07:18 -0600

    feat(ascents): remove ascent duration
    This is quite niche and will not be used in my test run with Black Hills
    Bouldering data.

--- schema/verify/climb/ascents.sql
+++ schema/verify/climb/ascents.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-SELECT id, climb_id, ascent_window, ascent_duration, first_ascent, members_complete, verified
+SELECT id, climb_id, ascent_window, first_ascent, members_complete, verified
     FROM climb.ascents
     WHERE FALSE;
 
