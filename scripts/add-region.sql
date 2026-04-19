b0e5f925604ac25d7edc7ab7729173e6e7e771c2
Logan Grosz <logan.grosz@gmail.com>
2026-04-19 17:06:37 -0600

    feat(scripts): add preview and confirmation
    This can probably be de-duped to some extent, but probably not necessary
    until I get the idea more ironed out.

--- scripts/add-region.sql
+++ scripts/add-region.sql
@@ -2,5 +2,22 @@
 \prompt 'Name: ' name
 \prompt 'Slug: ' slug
 
-insert into climb.regions (name, slug)
-values (:'name', :'slug');
+\echo 'Preview:'
+select
+  :'name' as name,
+  nullif(:'slug','') as slug;
+
+\prompt 'Insert? (y/N): ' confirm
+
+\if :confirm
+  insert into climb.regions (
+    name,
+    slug
+  ) values (
+    nullif(:'name', ''),
+    nullif(:'slug', '')
+  );
+\else
+  \echo 'Canceled'
+\endif
+
