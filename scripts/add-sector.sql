40a9ed9323af60d36ded98c1e18eb842942fbe84
Logan Grosz <logan.grosz@gmail.com>
2026-04-19 17:23:04 -0600

    feat(scripts): create add-sector script

---
+++ scripts/add-sector.sql
@@ -0,0 +1,46 @@
+\echo 'Add a new sector'
+
+\prompt 'Name: ' name
+\prompt 'Slug: ' slug
+\prompt 'Crag: ' crag
+
+-- Try direct lookup
+SELECT (
+  SELECT id::text
+  FROM climb.crags
+  WHERE id::text = :'crag'
+     OR slug = :'crag'
+  LIMIT 1
+) AS crag_id
+\gset
+
+-- Search for crag
+\if :{?crag_id}
+\else
+  \set crag_id `psql :'DBNAME' -Atc "select id||' '||name||' '||slug from climb.crags order by name" | fzf | awk '{print $1}'`
+\endif
+
+-- TODO Abort if crag id does not exist, because we it _will_ fail based on constraints
+
+\echo 'Preview:'
+select
+  :'name' as name,
+  nullif(:'slug','') as slug,
+  nullif(:'crag_id','')::uuid as crag_id;
+
+\prompt 'Insert? (y/N): ' confirm
+
+\if :confirm
+  insert into climb.sectors (
+    name,
+    slug,
+    crag_id
+  ) values (
+    nullif(:'name', ''),
+    nullif(:'slug', ''),
+    nullif(:'crag_id', '')::uuid
+  );
+\else
+  \echo 'Canceled'
+\endif
+
