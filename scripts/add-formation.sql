25f0c07f436c92d343c23f9d79008a07916b4e32
Logan Grosz <logan.grosz@gmail.com>
2026-04-19 21:23:46 -0600

    feat(scripts): create add-formation script

---
+++ scripts/add-formation.sql
@@ -0,0 +1,49 @@
+\echo 'Add a new formation'
+
+\prompt 'Name: ' name
+\prompt 'Slug: ' slug
+\prompt 'Parent (press enter)...' _
+
+-- Search for parent
+-- TODO I desperately need a way to split into lines for readability
+\set parent `psql :'DBNAME' -Atc "select format('region %s %s %s', id, name, slug) from climb.regions union all select format('crag %s %s %s', id, name, slug) from climb.crags union all select format('sector %s %s %s', id, name, slug) from climb.sectors order by 1 " | fzf`
+
+\set parent_type `echo :'parent' | awk '{print $1}'`
+\set parent_id `echo :'parent' | awk '{print $2}'`
+
+\echo 'Parent type:' :parent_type
+\echo 'Parent id:' :parent_id
+
+select :'parent_type' != '' as parent_is_valid
+\gset
+
+\if :parent_is_valid
+\else
+  -- Doesn't matter what this is because the id is going to be null
+  \set parent_type 'region'
+\endif
+
+\set parent_col :parent_type'_id'
+
+\echo 'Preview:'
+select
+  :'name' as name,
+  nullif(:'slug', '') as slug,
+  nullif(:'parent_id', '')::uuid as :parent_col;
+
+\prompt 'Insert? (y/N): ' confirm
+
+\if :confirm
+  insert into climb.formations (
+    name,
+    slug,
+    :parent_col
+  ) values (
+    nullif(:'name', ''),
+    nullif(:'slug', ''),
+    nullif(:'parent_id', '')::uuid
+  );
+\else
+  \echo 'Canceled'
+\endif
+
